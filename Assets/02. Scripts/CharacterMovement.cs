4209ec0882327e58373a2595273d1cd76f0a3924
Kim Ju-Yeong <wndud9678@gmail.com>
2025-05-22 18:31:20 +0900

    [MOD] CharacterMovement
    Input Manager를 이용한 입력 처리 변경

--- Assets/02. Scripts/CharacterMovement.cs
+++ Assets/02. Scripts/CharacterMovement.cs
@@ -4,32 +4,29 @@ public class CharacterMovement : MonoBehaviour
 {
     public float moveSpeed = 0.01f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        // transform.position = transform.position + Vector3.forward * moveSpeed;
+        /// Input System (Old - Legacy)
+        /// 입력값에 대한 약속된 시스템
+        /// 이동 -> WASD, 방향키
+        /// 점프 -> Space
+        /// 총쏘기 -> 마우스 왼쪽
 
-        if (Input.GetKey(KeyCode.W))    // 앞으로 가는 기능
-        {
+        /* if (Input.GetKey(KeyCode.W))    // 앞으로 가는 기능
             transform.position += Vector3.forward * (moveSpeed * Time.deltaTime);
-        }
         if (Input.GetKey(KeyCode.S))    // 뒤로 가는 기능
             transform.position += Vector3.back * (moveSpeed * Time.deltaTime);
-        {
-        }
         if (Input.GetKey(KeyCode.A))    // 왼쪽으로 가는 기능
-        {
             transform.position += Vector3.left * (moveSpeed * Time.deltaTime);
-        }
         if (Input.GetKey(KeyCode.D))     // 오른쪽으로 가는 기능
-        {
-            transform.position += Vector3.right * (moveSpeed * Time.deltaTime);
-        }
+            transform.position += Vector3.right * (moveSpeed * Time.deltaTime); */
+
+        float h = Input.GetAxis("Horizontal"); // -1 ~ 1
+        float v = Input.GetAxis("Vertical"); // -1 ~ 1
+
+        Vector3 dir = new(h, 0, v); // 방향을 나타내는 벡터
+        Debug.Log($"현재 입력 : {dir}");
+
+        transform.position += moveSpeed * Time.deltaTime * dir; // 이동하는 기능
     }
 }
